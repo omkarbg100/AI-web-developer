f66035c81b32bf1d7e7b718a9ff002cda5ada176
omkar gaikwad <147027748+omkarbg100@users.noreply.github.com>
2025-11-28 19:03:29 +0530

    backend

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -4,28 +4,33 @@ import redisClient from "../services/redis.service.js";
 
 export const authUser = async (req, res, next) => {
     try {
-        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+        let token;
+
+        // safely check if header exists
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+        } else if (req.cookies.token) {
+            token = req.cookies.token;
+        }
 
         if (!token) {
-            return res.status(401).send({ error: 'Unauthorized User' });
+            return res.status(401).send({ error: "Unauthorized User" });
         }
 
         const isBlackListed = await redisClient.get(token);
 
         if (isBlackListed) {
-
-            res.cookie('token', '');
-
-            return res.status(401).send({ error: 'Unauthorized User' });
+            res.cookie("token", "");
+            return res.status(401).send({ error: "Unauthorized User" });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
+
         next();
+
     } catch (error) {
-
         console.log(error);
-
-        res.status(401).send({ error: 'Unauthorized User' });
+        return res.status(401).send({ error: "Unauthorized User" });
     }
-}
+};
